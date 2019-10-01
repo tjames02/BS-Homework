661aee988c92fae47216231795c1bbaea1294994
TJ <tjames02@hamline.edu>
2019-09-30 21:10:10 -0500

    init

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> Proudly built by &nbsp;  
+        <a href= "https://tjames02.github.io">Tara James</a> &nbsp;
+         using React.js
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
